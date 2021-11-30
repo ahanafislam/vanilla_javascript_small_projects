1d29c0ddaeb7b0b494de9fadc6aafcc998783cca
Md Ahanaf Islam <ahanafislam@gmail.com>
2021-11-30 13:43:21 +0600

    Add game mode functionality

--- Number_Guessing_Game/static/js/app.js
+++ Number_Guessing_Game/static/js/app.js
@@ -1,9 +1,5 @@
 // Variable
-let minNum, maxNum, winningNum, guessLeft; 
-minNum = 1;
-maxNum = 10;
-winningNum = getRandomNumber(minNum, maxNum);
-guessLeft = 3;
+let minNum, maxNum, winningNum, guessLeft;
 
 // Ui Element
 const message = document.querySelector('#message');
@@ -13,18 +9,22 @@ const guessBtn = document.querySelector('#guess-btn');
 const guessLeftUi = document.querySelector('#guess-left');
 const minNumUI = document.querySelector('#min-num');
 const maxNumUI = document.querySelector('#max-num');
+const gameMode = document.querySelector("#game-mode");
 
 // Call Main Function
 main();
 
 // Create main function
 function main(){
-    minNumUI.textContent = minNum;
-    maxNumUI.textContent = maxNum;
+    changeGameMode();
+    guessLeft = 3;
+
     // Listen for guess
     guessForm.addEventListener('submit', getGameResult);
     // Listen for play again
     guessForm.addEventListener('mousedown',gameRestart);
+    // Listen for game mode
+    gameMode.addEventListener('change',changeGameMode);
     
 }
 
@@ -60,7 +60,8 @@ function getGameResult(e){
         }
     }
 
-    console.table({guessNum, winningNum});
+    let mode = gameMode.value
+    console.table({winningNum, maxNum, minNum, mode});
 }
 
 // Create gameOver Function
@@ -74,6 +75,7 @@ function gameOver(win,msg){
     guessBtn.classList.add('play-again');
     guessLeftUi.style.display = 'none';
     guessInput.value = '';
+    gameMode.hidden = true;
 
     msgDisplay(msg,color);
 }
@@ -99,9 +101,35 @@ function gameRestart(e){
         guessLeftUi.style.display = 'block';
         guessLeftUi.textContent = 'You can guess only 3 times!';
         msgDisplay("Type your guessing number","#333");
-        winningNum = getRandomNumber(minNum, maxNum);
+        changeGameMode();
         guessLeft = 3;
         guessBtn.classList.remove('play-again');
-        console.log("%c I'm clicked!!","color:red;");
+        gameMode.hidden = false;
+        // console.log("%c I'm clicked!!","color:red;");
     }
+}
+
+// For changing game mode
+function changeGameMode(){
+    if(gameMode.value === 'medium'){
+        setMaxMinVal(11,100);
+    }
+    else if(gameMode.value === 'hard'){
+        setMaxMinVal(101,500);
+    }
+    else{
+        setMaxMinVal(1,10);;
+    }
+    // console.log("%c I'm change!!"+gameMode.value,"color:red;");
+    // let mode = gameMode.value
+    // console.table({winningNum, maxNum, minNum, mode});
+}
+
+// For Set Game max and min number
+function setMaxMinVal(min, max){
+    minNum = min;
+    maxNum = max;
+    winningNum = getRandomNumber(minNum, maxNum);
+    minNumUI.textContent = minNum;
+    maxNumUI.textContent = maxNum;
 }
